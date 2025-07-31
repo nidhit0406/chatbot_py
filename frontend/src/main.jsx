3faa1326a9e1417bb86c99445e23ad40ce6f7d84
nidhi <nidhit0406@gmail.com>
2025-07-31 12:19:53 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,18 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
+const host = new URLSearchParams(window.location.search).get('host');
+
+const config = {
+  apiKey: import.meta.env.VITE_SHOPIFY_API_KEY,
+  host,
+  forceRedirect: true,
+};
+console.log('Shopify config:', config);
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter><App /></BrowserRouter>
+    <BrowserRouter config={config}><App /></BrowserRouter>
   </StrictMode>,
 )
