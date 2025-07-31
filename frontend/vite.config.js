803e3f5e64b06d299f0fe7522285b226a8869f9e
nidhi <nidhit0406@gmail.com>
2025-07-31 14:58:27 +0530

    test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -20,13 +20,7 @@ export default defineConfig({
       formats: ['iife']
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        }
-      }
+     
     }
   }
 });
