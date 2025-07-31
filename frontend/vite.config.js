377056475170bc0d046ba6171a6777d69c089502
nidhi <nidhit0406@gmail.com>
2025-07-31 15:46:18 +0530

    change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,34 +1,34 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-// import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-// })
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/widget.jsx'),
-      name: 'ChatbotWidget',
-      fileName: () => 'widget.js',
-      formats: ['iife'], // MUST be IIFE for browser <script> tag
-    },
-    rollupOptions: {
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-  },
-});
-;
+  plugins: [react(), tailwindcss()],
+})
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+// import path from 'path';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     lib: {
+//       entry: path.resolve(__dirname, 'src/widget.jsx'),
+//       name: 'ChatbotWidget',
+//       fileName: () => 'widget.js',
+//       formats: ['iife'], // MUST be IIFE for browser <script> tag
+//     },
+//     rollupOptions: {
+//       output: {
+//         globals: {
+//           react: 'React',
+//           'react-dom': 'ReactDOM',
+//         },
+//       },
+//     },
+//   },
+// });
+// ;
 
 
