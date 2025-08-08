d127388e80b7e739d95bb6ffae3c1b7807a6e5fe
nidhi <nidhit0406@gmail.com>
2025-08-08 12:15:35 +0530

    -- test

--- backend/app.py
+++ backend/app.py
@@ -75,7 +75,7 @@ def serve_widget_js():
   
   const config = {
     apiUrl: currentScript.getAttribute('data-api-url') || "https://n8nflow.byteztech.in/webhook/api/ask",
-    sessionApiUrl: currentScript.getAttribute('data-session-api-url') || "https://your-api.com/create-session",
+    sessionApiUrl:"http://103.39.131.9:8050/create-session",
     storeId: currentScript.getAttribute('data-store-id') || "116",
     welcomeMessage: currentScript.getAttribute('data-welcome-message') || "Hello! How can I help you today?",
     primaryColor: currentScript.getAttribute('data-primary-color') || "#8B5CF6",
